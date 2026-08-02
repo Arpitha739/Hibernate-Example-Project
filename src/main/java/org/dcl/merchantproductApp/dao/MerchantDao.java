db81523ff3bc02b69278280f62ee4c26b451d546
arpitha <arpithac15@gmail.com>
2026-08-02 21:46:37 +0530

    Added Merchant_Product_App Project

---
+++ src/main/java/org/dcl/merchantproductApp/dao/MerchantDao.java
@@ -0,0 +1,87 @@
+package org.dcl.merchantproductApp.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.dcl.merchantproductApp.dto.Merchant;
+
+public class MerchantDao
+{
+	EntityManagerFactory fac = Persistence.createEntityManagerFactory("dev");
+	EntityManager man = fac.createEntityManager();
+	
+	public Merchant saveMerchant(Merchant m) 
+	{
+		EntityTransaction tran = man.getTransaction();
+		tran.begin();
+		man.persist(m);
+		tran.commit();
+		return m;
+	}
+	
+	public Merchant updateMerchant(Merchant m)
+	{
+		
+		EntityTransaction tran=man.getTransaction();
+		tran.begin();
+		
+		Merchant mdb=man.find(Merchant.class, m.getId());
+		if(mdb!=null)
+		{
+			mdb.setName(m.getName());
+			mdb.setPhone(m.getPhone());
+			mdb.setGst_num(m.getGst_num());
+			mdb.setEmail(m.getEmail());
+			mdb.setPassword(m.getPassword());
+			
+			tran.commit();
+			return mdb;
+		}
+		
+		else
+		{
+			return null;
+		}
+	}
+	
+	public Merchant verifyMerchantByEmailAndPassword(String em, String pw) 
+	{
+		
+		Query q=man.createQuery("select m from Merchant m where m.email=?1 and m.password=?2");
+		q.setParameter(1, em);
+		q.setParameter(2, pw);
+		
+		try {
+			Merchant m=(Merchant) q.getSingleResult();
+			return m;
+		} catch (NoResultException e) 
+		{
+			return null;
+		}
+		
+	}
+
+	public Merchant findMerchantById(int id) {
+		return	man.find(Merchant.class, id);
+	}
+
+	public Merchant verifyMerchantByPhoneAndPassword(long ph, String pw) 
+	{
+		Query q=man.createQuery("select m from Merchant m where m.phone=?1 and m.password=?2");
+		q.setParameter(1, ph);
+		q.setParameter(2, pw);
+		
+		try {
+			Merchant m=(Merchant) q.getSingleResult();
+			return m;
+		} catch (NoResultException e) 
+		{
+			return null;
+		}
+		
+	}
+}
